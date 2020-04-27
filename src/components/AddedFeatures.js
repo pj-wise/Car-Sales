bc4a5a6894a4240bbd0f3e6ad37d058240eae56c
pearl-wise <pearl.w@salesboomerang.com>
2020-04-26 21:27:18 -0400

    MVP - trying refactor with hooks

--- src/components/AddedFeatures.js
+++ src/components/AddedFeatures.js
@@ -1,15 +1,21 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import AddedFeature from './AddedFeature';
+import { removeFeature } from '../actions/actions';
 
-const AddedFeatures = props => {
+const AddedFeatures = (props) => {
   return (
-    <div className="content">
+    <div className='content'>
       <h6>Added features:</h6>
       {props.car.features.length ? (
-        <ol type="1">
-          {props.car.features.map(item => (
-            <AddedFeature key={item.id} feature={item} />
+        <ol type='1'>
+          {props.car.features.map((item) => (
+            <AddedFeature
+              key={item.id}
+              feature={item}
+              removeFeature={props.removeFeature}
+            />
           ))}
         </ol>
       ) : (
@@ -19,4 +25,10 @@ const AddedFeatures = props => {
   );
 };
 
-export default AddedFeatures;
+const mapStateToProps = (state) => {
+  return {
+    car: state.car,
+  };
+};
+
+export default connect(mapStateToProps, { removeFeature })(AddedFeatures);
